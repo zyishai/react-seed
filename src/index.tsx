02b3785f4de5f89030a0a10a38210f607304e4b0
Yishai Zehavi <zehaviyishai@gmail.com>
2019-08-22 10:40:59 +0300

    changed single component structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './components/app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+async function bootstrap(component: any, element: HTMLElement | null) {
+    ReactDOM.render(component, element);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+}
+
+(async () => {
+    await bootstrap(<App />, document.getElementById('root'));
+})()

db83decd64d7f9f561e4f0464841fadaed2f07c9
Yishai Zehavi <zehaviyishai@gmail.com>
2019-08-22 20:12:37 +0300

    added additionals option when generating a component

--- plopfile.js
+++ plopfile.js
@@ -1,12 +1,23 @@
 // const NodePlop = require('node-plop').default();
-// const path = require('path');
+
+const createHOFActions = [{
+    type: 'add',
+    path: 'src/components/{{comp-name}}/{{comp-name}}.hoc.tsx',
+    templateFile: 'templates/component/additionals/hoc.hbs',
+}];
+
+const createContextActions = [{
+    type: 'add',
+    path: 'src/components/{{comp-name}}/{{comp-name}}.context.tsx',
+    templateFile: 'templates/component/additionals/context.hbs',
+}];
 
 /**
  * Plop config function 
  * @param {NodePlop} plop 
  */
 function plopConfig(plop) {
-    plop.setHelper('upperCase', txt => txt.charAt(0).toUpperCase() + txt.slice(1));
+    plop.setHelper('capitalize', txt => txt.charAt(0).toUpperCase() + txt.slice(1));
 
     plop.setGenerator('component', {
         description: 'Create functional component',
@@ -14,14 +25,58 @@ function plopConfig(plop) {
             type: 'input',
             name: 'comp-name',
             message: 'please type your component name'
+        }, {
+            type: 'checkbox',
+            name: 'additionals',
+            message: 'select additional services for your component',
+            choices: [
+                { name: 'Higher Order Function', value: 'hoc' },
+                { name: 'Context', value: 'context' }
+            ]
         }],
-        actions: [{
-            type: 'addMany',
-            destination: 'src/components/{{comp-name}}',
-            templateFiles: 'templates/component/*.*',
-            base: 'templates/component/'
-        }]
+        actions: function(data) {
+                const actions = [
+                    {
+                        type: 'addMany',
+                        destination: 'src/components/{{comp-name}}',
+                        templateFiles: 'templates/component/*.*',
+                        base: 'templates/component/',
+                        globOptions: {
+                            onlyFiles: true
+                        }
+                    }
+                ];
+
+                if (data.additionals.includes('hoc')) {
+                    actions.push(...createHOFActions);
+                }
+
+                if (data.additionals.includes('context')) {
+                    actions.push(...createContextActions);
+                }
+
+                return actions;
+        }
     });
+    plop.setGenerator('hoc', {
+        description: 'Add HOC to existing component',
+        prompts: [{
+            type: 'input',
+            name: 'comp-name',
+            message: 'component name'
+        }],
+        actions: createHOFActions
+    });
+    plop.setGenerator('context', {
+        description: 'Add a context object to existing component',
+        prompts: [{
+            type: 'input',
+            name: 'comp-name',
+            message: 'component name'
+        }],
+        actions: createContextActions
+    });
+
 }
 
 module.exports = plopConfig;

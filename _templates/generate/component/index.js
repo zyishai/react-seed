7a10fc7c57a3c597e77e884bacd8a9402ddc297c
Yishai Zehavi <zehaviyishai@gmail.com>
2019-08-28 19:36:16 +0300

    added component generator

---
+++ _templates/generate/component/index.js
@@ -0,0 +1,21 @@
+const enquirer = require('enquirer');
+
+module.exports = {
+    /**
+     * @param {{ prompter: enquirer, args: Object }} props
+     */
+    prompt: ({ prompter, args }) => {
+        return prompter.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'enter component name (dashed)',
+            skip: !!args.name
+        }, {
+            type: 'input',
+            name: 'module',
+            message: 'enter module name',
+            initial: 'root',
+            skip: (!!args.module || !!args.name)
+        }]);
+    }
+}
